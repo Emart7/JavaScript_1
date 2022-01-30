6db20fa6b4d4cc55295561b7ce9fd92e7bfaf32d
unknown <keyrus@COLMED0070.keyruscorp.key>
2022-01-29 22:45:40 -0500

    second commit

--- js/principal.js
+++ js/principal.js
@@ -17,12 +17,30 @@ var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura)
-tdImc.textContent = imc;
 
-console.log(imc);
-console.log(paciente);
-console.log(tdPeso);
-console.log(peso);
-console.log(tdAltura);
-console.log(altura);
+
+pesoEsValido = true;
+alturaEsValida = true;
+
+if ((peso < 0) || (peso > 1000)) {
+    console.log("Peso incorrecto.")
+    tdImc.textContent = "Peso incorrecto.";
+    pesoEsValido = false;
+}
+if ((altura < 0) || (altura > 3.0)) {
+    console.log("Altura incorrecto.")
+    tdImc.textContent = "Altura incorrecta.";
+    alturaEsValida = false;
+}
+
+if (pesoEsValido && alturaEsValida) {
+    var imc = peso / (altura * altura)
+    tdImc.textContent = imc;
+}
+
+// console.log(imc);
+// console.log(paciente);
+// console.log(tdPeso);
+// console.log(peso);
+// console.log(tdAltura);
+// console.log(altura);
