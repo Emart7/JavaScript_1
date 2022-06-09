f0416226136d6c63918eb0acea79136b52a9387b
unknown <keyrus@COLMED0070.keyruscorp.key>
2022-06-09 11:16:26 -0500

    second commit

---
+++ js/form.js
@@ -0,0 +1,39 @@
+var botonAdicionar = document.querySelector("#adicionar-paciente");
+
+botonAdicionar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adicionar");
+
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var tabla = document.querySelector("#tabla-pacientes");
+
+    //Creando etiquetas
+    pacienteTr = document.createElement("tr");
+
+    var nombreTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    //Creando contenido de las etiquetas
+    nombreTd.textContent = nombre;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = calcularImc(peso, altura);
+
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    tabla.appendChild(pacienteTr);
+
+});
